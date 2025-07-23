9adf737069c2d7244603ac9e3ed1646dea992129
Javier Alexander Echeverri Agudelo <alexanderems@outlook.com>
2025-07-23 09:17:58 -0500

    Header.tsx: added a functionality to give users the possibility to click the shopping cart icon to show cart, and to click anywhere in screen or press esc key to close it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,24 +5,62 @@
         This file contains the Header component to be render in App.tsx
         The header consist in a background image, a logo and a shopping cart
 */
-import type { Dispatch } from "react"
-import type { CartItem } from "../types"
-import type { CartActions } from "../reducers/product-reducer"
-import { MinusCircleIcon, PlusCircleIcon, XCircleIcon } from "@heroicons/react/16/solid"
-import { useMemo } from "react"
-import { formatCurrencyCol } from "../helpers"
+import type { Dispatch } from "react";
+import type { CartItem } from "../types";
+import type { CartActions } from "../reducers/product-reducer";
+import { MinusCircleIcon, PlusCircleIcon, XCircleIcon } from "@heroicons/react/16/solid";
+import { useMemo, useState, useEffect, useRef } from "react";
+import { formatCurrencyCol } from "../helpers";
 
 type HeaderProps = {
-    cart: CartItem[],
-    dispatch: Dispatch<CartActions>
-}
+    cart: CartItem[];
+    dispatch: Dispatch<CartActions>;
+};
 
-function Header( { cart, dispatch }: HeaderProps ) {
+function Header({ cart, dispatch }: HeaderProps) {
+
+    // state for cart open or close when user hits shopping cart image:
+    const [cartOpen, setCartOpen] = useState(false);
+
+    // We want the user to have the possibility of closing the cart table 
+    // hitting in any place of the screen or pressing "esc" key:
+    const cartRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            if (cartRef.current && !cartRef.current.contains(e.target as Node)) {
+                setCartOpen(false)
+            }
+        }
+
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (e.key === 'Escape') {
+                setCartOpen(false)
+            }
+        }
+
+        if (cartOpen) {
+            document.addEventListener('mousedown', handleClickOutside)
+            document.addEventListener('keydown', handleKeyDown)
+        }
+        else {
+            document.removeEventListener('mousedown', handleClickOutside)
+            document.removeEventListener('keydown', handleKeyDown)
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+            document.removeEventListener('keydown', handleKeyDown)
+        }
+    }, [cartOpen] )
 
     // Derived state:
-    const isEmpty   = useMemo( () => cart.length === 0, [cart] )
-    const cartTotal = useMemo( () =>
-                cart.reduce((total, item) => total + (item.quantity * item.price), 0), [cart])
+    const isEmpty = useMemo(() => cart.length === 0, [cart]);
+    const cartTotal = useMemo(
+        () =>
+            cart.reduce((total, item) => total + item.quantity * item.price, 0),
+        [cart]
+    );
 
     return (
         <>
@@ -46,36 +84,49 @@ function Header( { cart, dispatch }: HeaderProps ) {
                     </div>
 
                     {/* Shopping cart icon and hidden table:  */}
-                    <div className="relative group inline-block mt-4 md:mt-0 cursor-pointer">
+                    <div className="relative inline-block mt-4 md:mt-0 cursor-pointer" ref={ cartRef }>
                         <img
                             src="/carrito_compras.svg"
                             alt="Imagen carrito de compras"
                             className="w-18 h-18"
+                            onClick={() => setCartOpen(!cartOpen)}
                         />
-                        {/* Table: */}
-                        <div className="absolute left-1/2 -translate-x-1/2 md:left-auto md:right-0 md:translate-x-0 top-full mt-2 hidden group-hover:block bg-zinc-900 shadow-lg p-2 rounded-lg">
-                            {/* if cart is empty, show warning: */}
-                            { isEmpty ? (
-                                <p className="text-center text-white min-w-[15rem]">
-                                    Tu carrito está vacío
-                                </p>
-                            // if not empty, show table:
-                            ) : (
+                        {cartOpen && (
+                            /* Table. If need to return to hover, insert classes hidden and group-hover:block */
+                            <div className="absolute left-1/2 -translate-x-1/2 md:left-auto md:right-0 md:translate-x-0 top-full mt-2 bg-zinc-900 shadow-lg p-2 rounded-lg">
+                                {/* if cart is empty, show warning: */}
+                                {isEmpty ? (
+                                    <p className="text-center text-white min-w-[15rem]">
+                                        Tu carrito está vacío
+                                    </p>
+                                ) : (
+                                    // if not empty, show table:
                                     <div className="max-w-[28rem] overflow-auto text-xs p-4">
                                         <table className="w-full table-fixed text-white">
                                             <thead>
                                                 <tr className="text-left">
-                                                    <th className="w-20">Imagen</th>
-                                                    <th className="w-28">Nombre</th>
-                                                    <th className="w-24">Precio</th>
-                                                    <th className="w-18">Cantidad</th>
+                                                    <th className="w-20">
+                                                        Imagen
+                                                    </th>
+                                                    <th className="w-28">
+                                                        Nombre
+                                                    </th>
+                                                    <th className="w-24">
+                                                        Precio
+                                                    </th>
+                                                    <th className="w-18">
+                                                        Cantidad
+                                                    </th>
                                                     <th className="w-8"></th>
                                                 </tr>
                                             </thead>
 
                                             <tbody>
-                                                { cart.map(product => (
-                                                    <tr key={ product.id } className="">
+                                                {cart.map((product) => (
+                                                    <tr
+                                                        key={product.id}
+                                                        className=""
+                                                    >
                                                         <td>
                                                             <img
                                                                 className="w-12 h-auto"
@@ -83,28 +134,26 @@ function Header( { cart, dispatch }: HeaderProps ) {
                                                                 alt="Imagen de un producto"
                                                             />
                                                         </td>
-                                                        <td>{ product.name }</td>
-                                                        <td className="font-black">{ `${formatCurrencyCol(product.price)}` }</td>
+                                                        <td>{product.name}</td>
+                                                        <td className="font-black">{`${formatCurrencyCol(product.price)}`}</td>
                                                         <td>
                                                             <div className="flex items-center gap-1">
                                                                 <button
                                                                     type="button"
-                                                                    onClick={ () => dispatch({ type: 'decrease-quantity', payload: { id: product.id } }) }
+                                                                    onClick={ () => dispatch({ type: "decrease-quantity", payload: { id: product.id } }) }
                                                                 >
-                                                                    <MinusCircleIcon
-                                                                        className="h-4 w-4 text-violet-600 cursor-pointer"
-                                                                    /> 
+                                                                    <MinusCircleIcon className="h-4 w-4 text-violet-600 cursor-pointer" />
                                                                 </button>
 
-                                                                { product.quantity }
+                                                                {
+                                                                    product.quantity
+                                                                }
 
                                                                 <button
                                                                     type="button"
-                                                                    onClick={ () => dispatch({ type: 'increase-quantity', payload: { id: product.id } }) }
+                                                                    onClick={ () => dispatch({ type: "increase-quantity", payload: { id: product.id } }) }
                                                                 >
-                                                                    <PlusCircleIcon
-                                                                        className="h-4 w-4 text-violet-600 cursor-pointer"
-                                                                    />
+                                                                    <PlusCircleIcon className="h-4 w-4 text-violet-600 cursor-pointer" />
                                                                 </button>
                                                             </div>
                                                         </td>
@@ -112,40 +161,39 @@ function Header( { cart, dispatch }: HeaderProps ) {
                                                             <div className="flex items-center gap-2">
                                                                 <button
                                                                     type="button"
-                                                                    onClick={ () => dispatch({ type: 'remove-from-cart', payload: { id: product.id } }) }
+                                                                    onClick={ () => dispatch({ type: "remove-from-cart", payload: { id: product.id } }) }
                                                                 >
-                                                                    <XCircleIcon
-                                                                        className="h-4 w-4 text-red-500 cursor-pointer"
-                                                                    />
+                                                                    <XCircleIcon className="h-4 w-4 text-red-500 cursor-pointer" />
                                                                 </button>
                                                             </div>
                                                         </td>
-                                                   </tr> 
-                                                )) }
+                                                    </tr>
+                                                ))}
                                             </tbody>
                                         </table>
 
-                                            <p className="text-center text-white text-lg m-6">
-                                                Total a pagar: {`${" "}`}
-                                                <span className="font-black text-white text-xl">
-                                                    { `${formatCurrencyCol(cartTotal)}` }
-                                                </span>
-                                            </p>
-                                            <button
-                                                type="button"
-                                                title="Vaciar carrito"
-                                                className="bg-violet-600 hover:bg-violet-800 w-full p-2 font-bold uppercase text-white cursor-pointer rounded-md"
-                                                onClick={ () => dispatch( { type: 'clean-cart' } ) }
-                                            >
-                                                Vaciar el carrito
-                                            </button>
+                                        <p className="text-center text-white text-lg m-6">
+                                            Total a pagar: {`${" "}`}
+                                            <span className="font-black text-white text-xl">
+                                                { `${formatCurrencyCol(cartTotal)}` }
+                                            </span>
+                                        </p>
+                                        <button
+                                            type="button"
+                                            title="Vaciar carrito"
+                                            className="bg-violet-600 hover:bg-violet-800 w-full p-2 font-bold uppercase text-white cursor-pointer rounded-md"
+                                            onClick={ () => dispatch({ type: "clean-cart" }) }
+                                        >
+                                            Vaciar el carrito
+                                        </button>
                                     </div>
                                 )}
-                        </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
         </>
-    )
+    );
 }
 export default Header;
